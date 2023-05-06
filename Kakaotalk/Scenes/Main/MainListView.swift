9baf07964b762ba44bebe0010fc3b76510f75539
Min Kwon <min.kwon14@gmail.com>
2023-05-05 20:08:28 -0400

    fixed file issue

--- Kakaotalk/Scenes/Main/MainListView.swift
+++ Kakaotalk/Scenes/Main/MainListView.swift
@@ -2,19 +2,49 @@
 //  MainListView.swift
 //  Kakaotalk
 //
-//  Created by 강성찬 on 2023-05-01.
+//  Created by 강성찬 on 2023-05-02.
 //
 
 import SwiftUI
 
 struct MainListView: View {
+    
+    var user: User
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack(spacing: 24) {
+            CircledImage(
+                title: user.thumbnailPictureURL,
+                width: 48,
+                height: 48,
+                placeholderSysname: "person"
+            )
+     
+            VStack {
+                HStack {
+                    TextSection(
+                        title: user.fullName,
+                        font: .smallBold,
+                        textColor: .black
+                    )
+                    Spacer()
+                }
+                HStack {
+                    TextSection(
+                        title: "\(user.location.city)\t\(user.location.state)",
+                        font: .small,
+                                                textColor: .gray
+                    )
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
-        MainListView()
+        MainListView(user: .mockUser)
     }
 }
